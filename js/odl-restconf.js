e976f1beaa22dc12aa517d56aabbd6248f26ea59
jensenzhang <jingxuan.n.zhang@gmail.com>
2016-04-19 14:35:35 +0800

    Update layout of web ui
    Add navbar and footer for web ui. Use two columns layout. Refine
    css style. Fix crash bug caused by odl-restconf forward.
    
    Signed-off-by: jensenzhang <jingxuan.n.zhang@gmail.com>

--- js/odl-restconf.js
+++ js/odl-restconf.js
@@ -3,12 +3,16 @@ var request = require( 'request' );
 
 exports.forward = function( req, res ) {
   var opts = { url: config.odl_server_path + "/restconf/" + req.params[0]
-             , headers: { 'Authorization': 'Bearer ' + config.oauth_token } };
+             , headers: { 'Authorization': 'Basic ' + config.oauth_token } };
 
   request.get( opts
              , function( error, response, data ) {
-                 res.writeHead( 200, { 'Content-Type': 'application/json' } );
-                 res.write( data );
-                 res.end();
+                 if ( !error && response.statusCode == 200 ) {
+                     res.writeHead( 200, { 'Content-Type': 'application/json' } );
+                     res.write( data );
+                     res.end();
+                 } else {
+                     console.error( "read data failed" );
+                 }
                } );
 }
